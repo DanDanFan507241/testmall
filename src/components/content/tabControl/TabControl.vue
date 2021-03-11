d26612348b6c38e464168cd7fd46de9972b3ee03
DanDanFan <2582620878@qq.com>
2021-03-11 21:14:49 +0800

    提交文件

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tabcontrol">
+    <div
+      v-for="(item,index) in title"
+      :key="index"
+      class="tabcontrol-item"
+      :class="{active: index === currentIndex}"
+      @click="tabClick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  props: {
+    title: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods: {
+    tabClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .tabcontrol {
+    display: flex;
+    font-style: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+  }
+  .tabcontrol-item {
+    flex: 1;
+    text-align: center;
+  }
+  .tabcontrol-item span {
+    padding: 5px;
+  }
+  .active {
+    color: var(--color-high-text);
+   
+  }
+  .active span {
+    border-bottom: 3px solid var(--color-high-text);
+  }
+</style>
--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -1,18 +1,134 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :title="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import RecommendView from "../../view/home/childrenComponents/RecommendView";
+import NavBar from "../../components/common/navbar/NavBar";
+import { getHomeMultidata, getHomeGoods } from "../../network/home";
+import HomeSwiper from "./childrenComponents/HomeSwiper";
+import FeatureView from "./childrenComponents/FeatureView";
+import TabControl from "../../components/content/tabControl/TabControl";
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: {
+          page: 0,
+          list: [],
+        },
+        new: {
+          page: 0,
+          list: [],
+        },
+        sell: {
+          page: 0,
+          list: [],
+        },
+      },
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+  },
+  created() {
+    //1.请求多个数据
+    this.getHomeMultidata();
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
-
+  #home {
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
 </style>
